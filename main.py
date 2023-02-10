96b867c50fd53c5220ac4d1dbc787d73b0d7445f
lynah2 <lynahafid2@gmail.com>
2023-02-10 21:01:34 +0100

    memo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import re
 import datetime
+from supermemo2 import SMTwo
+
 
 BACKGROUND_COLOR = "#B1DDC6"
 import random
@@ -12,9 +14,6 @@ current_card = {}
 try: # try running this line of code
     data = pd.read_csv("frToEng.csv")
 except FileNotFoundError:
-    # If for the first time we are running it
-    # the .csv file might not be present
-    # and FileNotFoundError might pop up
     data = pd.read_csv("frToEng.csv") 
 else:
     data = pd.read_csv("frToEng.csv")
@@ -28,36 +27,70 @@ def is_comprehensible(word_f, word_e):
 data = data[data.apply(lambda x: is_comprehensible(x['French'], x['English']), axis=1)]
 data.to_csv("frToEng.csv", index=False)
 
-if 'lastRevised' not in data.columns:
+if 'lastRevised' not in data.columns:   #updated in next_card
     data['lastRevised'] = 0
-if 'learned' not in data.columns:
-    data['learned'] = 'n'
-if 'nextTime' not in data.columns:
-    data['nextTime'] = 0
+
+if 'repetition' not in data.columns:   #updated in next_card
+    data['repetition'] = 0
+data['repetition'] = data['repetition'].astype(int)
+
+if 'correct' not in data.columns:   #updated if known
+    data['correct'] = 0
+data['correct'] = data['correct'].astype(int)
+
+now = datetime.datetime.now().strftime("%Y-%m-%d")
+
+if 'nextTime' not in data.columns:   #updated in next_card assuming user got it wrong then update in is_known if known
+    data['nextTime'] = now 
+
+if 'responseQuality' not in data.columns:   #updated in next_card assuming user got it wrong then update in is_known if known
+    data['responseQuality'] = 0
+data['responseQuality'] = data['responseQuality'].astype(int)
+
+
+        
 to_learn = data.to_dict(orient="records")
 now = datetime.datetime.now().strftime("%Y-%m-%d")
 
+print(len(to_learn))
+
+
 Today = [d for d in to_learn if d['nextTime'] == now]
+print(len(Today))
 
-print(Today)
+to_learn = [x for x in to_learn if x not in Today]
+print(len(to_learn))
 
+#print(Today)
+i = 0
 
 #------------------------ Generating a English word ----------
 
 def next_card():
     global current_card, flip_timer
+    global i
     window.after_cancel(flip_timer)
+    i += 1
+
+    print(i)
     
     if Today:
         current_card = random.choice(Today)
+        Today.remove(current_card)
     elif to_learn:
         current_card = random.choice(to_learn)
+        to_learn.remove(current_card)
 
     # Update the relevant row
-    data.loc[data['French'] == current_card['French'], 'last_revised'] = datetime.datetime.now().strftime("%Y-%m-%d")
-    data.loc[data['French'] == current_card['French'], 'learned'] = 'n'
+    data.loc[data['English'] == current_card['English'], 'lastRevised'] = datetime.datetime.now().strftime("%Y-%m-%d")
+    data.loc[data['English'] == current_card['English'], 'repetition'] += 1  
     
-    data.loc[data['French'] == current_card['French'], 'nextTime'] = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    data.loc[data['English'] == current_card['English'], 'responseQuality'] = calculate_responseQuality(current_card['correct'], current_card['repetition'])
+    
+    if current_card['repetition'] == 0: 
+        data.loc[data['English'] == current_card['English'], 'nextTime'] = review = SMTwo.first_review(0,now).review_date
+    else:
+        data.loc[data['English'] == current_card['English'], 'nextTime'] = SMTwo(review.easiness, review.interval, review.repetitions).review(current_card['responseQuality'], now).review_date
     # Write the DataFrame back to the file
     data.to_csv("frToEng.csv", index=False)
 
@@ -73,20 +106,42 @@ def flip_card():
     canvas.itemconfig(card_background, image=card_back_img)
 
 def is_known():
-    if current_card in Today:
-        Today.remove(current_card)
-    elif current_card in to_learn:
-        to_learn.remove(current_card)
+    
     # Load the CSV file into a DataFrame
     data = pd.read_csv("frToEng.csv")
     # Update the relevant row
-    data.loc[data['French'] == current_card['French'], 'learned'] = 'y'
-    data.loc[data['French'] == current_card['French'], 'nextTime'] = (datetime.datetime.now() + datetime.timedelta(days=7))
+    data.loc[data['English'] == current_card['English'], 'correct'] += 1
+    data.loc[data['English'] == current_card['English'], 'responseQuality'] = calculate_responseQuality(current_card['correct'], current_card['repetition'])
+
+    if current_card['repetition'] == 0: 
+        data.loc[data['English'] == current_card['English'], 'nextTime'] = review = SMTwo.first_review(0,now).review_date
+    else:
+        data.loc[data['English'] == current_card['English'], 'nextTime'] = SMTwo(review.easiness, review.interval, review.repetitions).review(current_card['responseQuality'], now).review_date
 
     data.to_csv("frToEng.csv", index=False)
     # index = false discrads the index numbers
     next_card()
     
+def calculate_responseQuality(correct, repetition):
+    if repetition != 0:
+        res = correct / repetition
+    else:
+        res = 0
+    if res == 1:
+        return 5
+    elif res > 0.75:
+        return 4
+    elif res > 0.5:
+        return 3
+    elif res > 0.25:
+        return 2
+    elif res == 0:
+        return 0
+    else:   
+        return 1 
+
+
+
 
 
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+import supermemo2
+
+
+supermemo2
+
+
